9b7d672ebe787e967ec15353b2acf48a1238fbf6
sankesh reddy <sankeshreddy@gmail.com>
2021-05-08 20:47:09 +0530

    counter app Business logic

--- three-state-counter/src/App.js
+++ three-state-counter/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 
 function App(){
+
+  const [count, setCount] = useState(0);
   return(
-    <div>
-      <h1>Hello</h1>
+    <div className="App">
+    <header>
+      <h1>Counter app using State/Hooks</h1>
+    </header>
+    <h2>Current value of Count is {count}</h2>
+    {/* <button onClick={() => setCount(0)}>Reset Counter</button>
+    <button onClick={() => setCount(count + 1)}>Increase Counter</button>
+    <button onClick= {() => setCount(count - 1)}>Decrease Counter</button> */}
+    <button onClick={() => setCount(0)}>Reset Counter</button>
+    <button onClick={() => (count>=10)?"":setCount(count + 1)}>Increase Counter</button>
+    <button onClick= {() => (count>=1)?setCount(count-1):count}>Decrease Counter</button>
     </div>
   )
 }
