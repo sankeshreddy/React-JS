64530110b44333f4a71d08709fdd83b705ed49ae
sankesh reddy <sankeshreddy@gmail.com>
2021-05-13 22:20:22 +0530

    icons required for the game

--- four-tictactoe/src/components/Icon.js
+++ four-tictactoe/src/components/Icon.js
@@ -3,12 +3,22 @@ import {FaTimes, FaPen, FaRegCircle} from 'react-icons/fa';
 
 const Icon = ({name}) => {
 
-    return (
+switch (name) {
+        case "circle":
+          return <FaRegCircle className="icons" />;
+        case "cross":
+          return <FaTimes className="icons" />;
+        default:
+          return <FaPen className="icons" />;
+      }
 
-        <h1>
-            <FaTimes className = "icon"/>
-        </h1>
-    );
+
+    // return (
+
+    //     <h1>
+    //         <FaTimes className = "icon"/>
+    //     </h1>
+    // );
 };
 
 export default Icon;
